4211466c1f6dfa81c89e7c42beddd7e26944a7fd
Anton Burduzha <anton.burduzha@hotmail.com>
2016-08-22 13:08:29 +0300

    realise heapchat-clone navbar

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ gulp.task('index', function () {
        .pipe(gulp.dest('dist'))
 });
 
+gulp.task('images', function () {
+    gulp.src('src/img/*.jpg')
+        .pipe(gulp.dest('dist/img'))
+});
+
 gulp.task('sass-lint', function () {
     return gulp.src(['src/**/*.scss'])
         .pipe(sassLint({
@@ -31,9 +36,15 @@ gulp.task('watch',function () {
     gulp.watch('src/**/*.scss', ['sass']);
 });
 
-gulp.task('vendor', function () {
+gulp.task('vendor-css-bootstrap', function () {
     gulp.src('node_modules/bootstrap/dist/css/bootstrap.css')
         .pipe(gulp.dest('dist/css'));
-})
+});
 
-gulp.task('build', ['vendor', 'index', 'sass']);
+gulp.task('vendor-js-bootstrap', function () {
+    gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('build', ['vendor-css-bootstrap', 'vendor-js-bootstrap', 'index', 'sass', 'images']);
